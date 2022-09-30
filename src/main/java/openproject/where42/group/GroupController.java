4f0a01bb4515202466b2e30a0dc462a9180a9fc3
highcho <highjcho@gmail.com>
2022-09-30 11:57:56 +0900

    [STYLE] : make group util pakage, [FEAT] : add update group name method

--- src/main/java/openproject/where42/group/GroupController.java
+++ src/main/java/openproject/where42/group/GroupController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class GroupController {
     private final GroupService groupService;
 
-    @GetMapping("/groups/new")
+    @GetMapping("/groups/new") // 그룹 이름 수정할 때도 이걸 활용하려고 하는데, 그룹 아이디를 보내줄 수 있는지 아니면 변수가 필요한지?
     public String createForm(Model model) {
         model.addAttribute("form", new GroupForm());
         return "groups/createGroupForm";
@@ -21,10 +21,16 @@ public class GroupController {
 
     @PostMapping("/groups/new")
     public String createGroup(GroupForm form) {
-        groupService.saveGroup(form.getGroupName(), form.getMemberId());
+        groupService.saveGroup(form.getGroupName());
         return "redirect:/";
     }
-    @PostMapping("/groups/{groupId}/delete") // delete이런거 url로 하지 말랬는디.. 예외처리는 다 서비스에서 하는건가?
+
+    @PostMapping("/groups/setting")
+    public String updateGroupName(Long groupId, GroupForm form) {
+        groupService.updateGroupName(groupId, form.getGroupName());
+        return "redirect:/";
+    }
+    @PostMapping("/groups/{groupId}/delete") // delete이런거 url로 하지 말랬는디..뭐 어떤식으로 구성해야할지 몰게씀.. 예외처리는 다 서비스에서 하는건가?
     public String deleteGroup(@PathVariable("groupId") Long groupId) {
         groupService.deleteGroup(groupId);
         return "redirect:/";
--- src/main/java/openproject/where42/group/domain/Groups.java
+++ src/main/java/openproject/where42/group/domain/Groups.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SequenceGenerator(
         name = "GROUPS_SEQ_GENERATOR",
@@ -34,19 +34,11 @@ public class Groups {
     @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<GroupMember> groupMembers = new ArrayList<>();
 
-    public Groups (String groupName, Member owner) { // 이렇게 생성자를 주면 id값이 제대로 발생할까?
+    public Groups (String groupName) { // 이렇게 생성자를 주면 id값이 제대로 발생할까?
         this.groupName = groupName;
-        this.owner = owner;
     }
 
-    public List<String> findAllFriends() { //groupMembers를 객체가 아닌 string으로 가지고 있는건? 아니면 이런식의 변환이 필요함, 프론트에 넘겨주는 것
-        List<String> friends = new ArrayList<String>();
-        if (groupMembers.isEmpty()) // 예외처리 해줘야 하나? 아니면 없으면 알아서 널인가?
-            return null;
-        for (GroupMember f : groupMembers) {
-            friends.add(f.getFriendName());
-        }
-        return friends;
+    public void updateGroupName(String groupName) {
+        this.groupName = groupName;
     }
-
 }
