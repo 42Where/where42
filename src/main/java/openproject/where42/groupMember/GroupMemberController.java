2c0c3e4b485033b2957a89ae59fae59f9418f19f
sunghkim <az0856@naver.com>
2022-10-06 20:28:25 +0900

    [FEAT] : add GroupMember Controller logic

--- src/main/java/openproject/where42/groupMember/GroupMemberController.java
+++ src/main/java/openproject/where42/groupMember/GroupMemberController.java
@@ -1,12 +1,18 @@
 package openproject.where42.groupMember;
 
 import lombok.RequiredArgsConstructor;
+import openproject.where42.group.GroupService;
+import openproject.where42.group.domain.Groups;
 import openproject.where42.groupMember.domain.GroupMember;
 import openproject.where42.groupMember.dto.GroupMemberForm;
+import openproject.where42.member.MemberService;
+import openproject.where42.member.repository.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -16,6 +22,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GroupMemberController {
 	public final GroupMemberService groupMemberService;
+	public final GroupService groupService;
 
 	@GetMapping("/groupMember/new")
 	public String createForm(Model model) {
@@ -31,11 +38,43 @@ public class GroupMemberController {
 		return "redirect:/"; //영한씨가 홈에 보내라고 했다..
 	}
 
-	@GetMapping("/groupMember/news")
+	@GetMapping("/groupMembers/new")
 	public String multicreateForm(Model model) {
-//		List<GroupMember> list;
-//		model.addAttribute("groupMemberForms", list);
+		List<GroupMember> list = null; // 이거 리스트 보내면 프론트에서 채워 넣을 수 있는건가...?
+		model.addAttribute("groupMemberForms", list);
 		return "???";
+	}
+
+	@PostMapping("/groupMembers/new")
+	public String muticreate(@Valid List<GroupMember> list) {
+		groupMemberService.multiSaveGroupMember(list);
+		return "redirect:/";
+	}
+
+	@GetMapping("/groupMember/{groupId}/delete")
+	public String deleteGroupMember(@PathVariable("groupId") Long groupId, Model model) {
+//		List<GroupMember> groupMembers = groupMemberService.groupMemberList(groupId); --> 그룹멤버 리스트를 줘야 화면에 뛰움
+//		model.addAttribute("groupMembers", groupMembers);
+		return "???";
+	}
+
+	@PostMapping("/groupMember/{groupId}/delete")
+	public String deleteGroupMember(GroupMember groupMember) {
+		groupMemberService.deleteGroupMember(groupMember);
+		return "redirect:/";
+	}
+
+	@GetMapping("/groupMembers/{groupId}/delete")
+	public String deleteGroupMembers(@PathVariable("groupId") Long groupId, Model model) {
+//		List<GroupMember> groupMembers = groupMemberService.groupMemberList(groupId); --> 그룹멤버 리스트를 줘야 화면에 뛰움
+//		model.addAttribute("groupMembers", groupMembers);
+		return "???";
+	}
+
+	@PostMapping("/groupMembers/{groupId}/delete")
+	public String deleteGroupMembers(List<GroupMember> groupMembers) {
+		groupMemberService.deleteGroupMembers(groupMembers);
+		return "redirect:/";
 	}
 
 }
