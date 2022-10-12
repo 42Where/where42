262fbc241fd7d6bb1abaef74ce4c52d1345b1c7c
zoovely <yon041128@naver.com>
2022-10-12 20:36:13 +0900

    [STYLE] : change function structure

--- React/where42/src/Login.js
+++ React/where42/src/Login.js
@@ -9,30 +9,35 @@ function Login() {
     const isMobile = useMediaQuery({ query: '(max-width: 480px)'})
     const isTablet = useMediaQuery({ query: '(min-width: 481px) and (max-width: 1023px)'})
     const isDesktop = useMediaQuery({ query: '(min-width: 1024px)'})
-
-    return (
-        <div className="Login">
-            {isMobile && <div id="Mobile"><Common/></div>}
-            {isTablet && <div id="Tablet"><Common/></div>}
-            {isDesktop && <div id="Desktop"><Common/></div>}
-        </div>
-    );
-}
-function Common() {
     const [modal, setModal] = useState(0);
-    function clickDown() {
-        const button = document.getElementById('Login-button');
-        button.style = "background-image: url('img/login_button_click.svg'); background-size: contain";
+
+    function Common() {
+        function clickDown() {
+            const button = document.getElementById('Login-button');
+            button.style = "background-image: url('img/login_button_click.svg'); background-size: contain";
+        }
+        function clickUp() {
+            const button = document.getElementById('Login-button');
+            button.style = "background-image: url('img/login_button.svg')";
+            window.location.href = 'https://profile.intra.42.fr/users/sojoo';
+        }
+       
+        return (
+            <div id="Common">
+                <div id="Main">
+                    <button id="Wiki" onClick={()=>{setModal(1)}} ></button>
+                    <div id="Logo">
+                        <img src="img/logo.svg" alt="logo"></img>
+                    </div>
+                   <div id="Character">
+                        <img src="img/character.svg" alt="character"></img>
+                    </div>
+                    <button id="Login-button" onMouseDown={clickDown} onMouseUp={clickUp}></button>
+                </div>
+            </div>
+        )
     }
-    function clickUp() {
-        const button = document.getElementById('Login-button');
-        button.style = "background-image: url('img/login_button.svg')";
-        window.location.href = 'https://profile.intra.42.fr/users/sojoo';
-    }
-    function contactClick() {
-        window.open('https://github.com/5ganization', '_blank');
-    }
-   
+    
     function Modal() {
         return (
             <div id="Modal">
@@ -42,24 +47,15 @@ function Common() {
             </div>
         )
     }
+
     return (
-        <div id="Common">
+        <div className="Login">
             {modal === 1 ? <Modal/> : null}
-            <div id="Icon">
-                <button id="contact" onClick={contactClick}></button>
-                <button id="wiki" onClick={()=>{setModal(1)}} ></button>
-            </div>
-            <div id="Main">
-                <div id="Logo">
-                    <img src="img/logo.svg" alt="logo"></img>
-                </div>
-               <div id="Character">
-                    <img src="img/character.svg" alt="character"></img>
-                </div>
-                <button id="Login-button" onMouseDown={clickDown} onMouseUp={clickUp}></button>
-            </div>
+            {isMobile && <div id="Mobile"><Common/></div>}
+            {isTablet && <div id="Tablet"><Common/></div>}
+            {isDesktop && <div id="Desktop"><Common/></div>}
         </div>
-    )
+    );
 }
 
 export default Login;
