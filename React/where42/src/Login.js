dad289b131bf7e66aed7f7f0f6a3c492f5412f9a
zoovely <yon041128@naver.com>
2022-10-12 16:34:23 +0900

    [FEAT] : Fix Media Query

--- React/where42/src/Login.js
+++ React/where42/src/Login.js
@@ -1,17 +1,20 @@
 // import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import './Login.css'
+import './Login_Mobile.css'
+import './Login_Tablet.css'
 import './Login_Desktop.css'
-import './Login_Laptop.css'
 
 function Login() {
-    const isDesktop = useMediaQuery({ query: '(min-width: 2561px)'})
-    const isLaptop = useMediaQuery({ query: '(max-width: 2560px)'})
+    const isMobile = useMediaQuery({ query: '(max-width: 480px)'})
+    const isTablet = useMediaQuery({ query: '(min-width: 481px) and (max-width: 1023px)'})
+    const isDesktop = useMediaQuery({ query: '(min-width: 1024px)'})
 
     return (
         <div className="Login">
+            {isMobile && <div id="Mobile"><Common/></div>}
+            {isTablet && <div id="Tablet"><Common/></div>}
             {isDesktop && <div id="Desktop"><Common/></div>}
-            {isLaptop && <div id="Laptop"><Common/></div>}
         </div>
     );
 }
@@ -27,25 +30,12 @@ function Common() {
         button.style = "background-image: url('img/login_button.svg')";
         window.location.href = 'https://profile.intra.42.fr/users/sojoo';
     }
-    function contactClick() {
-        window.open('https://github.com/5ganization', '_blank');
-    }
-    // function wikiClick() {
-    //     // #Main을 날리고 <div>를 올려서 텍스트로 사용방법 보여주기
-    //     // 우리는 친구 자리 찾기 페이지다~
-    //     // 로그인 하시면 메인에 친구 자리 뜨고~ 친구 검색해서 등록도 되고~
-    //     // 그룹 설정도 된다~~ 출퇴근을 위한 개인정보가 필요하다~
-    //     // 바로 가입하시라~~ 저희는 where42이다~
-    // }
 
     return (
         <div id="Common">
             {/* {modal === 1 ? <Modal/> : null} */}
-            <div id="Icon">
-                <button id="contact" onClick={contactClick}></button>
-                <button id="wiki" /*onClick={()=>{setModal(1)}*/></button>
-            </div>
             <div id="Main">
+                <button id="Wiki" /*onClick={()=>{setModal(1)}*/></button>
                 <div id="Logo">
                     <img src="img/logo.svg" alt="logo"></img>
                 </div>
