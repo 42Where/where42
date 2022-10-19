d1b39a1de9df0d508e0d4fc40d6da601c1c12dba
mulloc1 <skybgk1@gmail.com>
2022-10-19 15:27:22 +0900

    [REFACTOR] : Planet 열거형 개포(1), 서초(2)로 설정

--- src/main/java/openproject/where42/member/domain/enums/Planet.java
+++ src/main/java/openproject/where42/member/domain/enums/Planet.java
@@ -1,5 +1,15 @@
 package openproject.where42.member.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Planet {
-    gaepo, seocho;
+    gaepo(1), seocho(2);
+
+    Planet(Integer value) {
+        this.value = value;
+    }
+
+    private final Integer value;
+
 }
