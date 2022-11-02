3922acf1420494be92b0061b0fa94ca28d1633a7
lamPolar <lovast7@gmail.com>
2022-11-02 11:36:14 +0900

    [REFACTOR] change Profile-circle div & sample.json

--- React/where42/src/Main/profile.js
+++ React/where42/src/Main/profile.js
@@ -46,29 +46,27 @@ function GetBackgroundImage(inOutState, spot)
 
 function CombineLocate(locate, inOutState){
     let position = "";
-    // if (inOutState === "2")
-    // {
-    //     position += "자리정보없음";
-    //     return position;
-    // }
-    // else if (inOutState === "1")
-    // {
-    //     position += 
-    // }
-    if (locate.planet === "0")
-        return position;
-    if (locate.planet === "1")
-        position += '개포 ';
-    else if (locate.planet === "2")
-        position += '서초 ';
-    if (locate.floor > "0")
-        position += locate.floor.toString() + '층 ';
-    if (locate.floor === "-1")
-        position += '지하 1층 ';
-    if (locate.cluster !== "0")
-        position += locate.cluster.toString() + '클러스터 '; // '클러스터' 라고 쓸건지?
-    if (locate.spot !== null)
-        position += locate.spot.toString();
+    if (inOutState === "2")
+        position = "자리 정보 없음";
+    else if (inOutState === "0")
+        position = "퇴근";
+    else
+    {
+        if (locate.planet === "0")
+            position = "클러스터 내";
+        if (locate.planet === "1")
+            position += '개포 ';
+        else if (locate.planet === "2")
+            position += '서초 ';
+        if (locate.floor > "0")
+            position += locate.floor.toString() + '층 ';
+        if (locate.floor === "-1")
+            position += '지하 1층 ';
+        if (locate.cluster !== "0")
+            position += locate.cluster.toString() + '클 ';
+        if (locate.spot !== null)
+            position += locate.spot.toString();
+    }
     return position;
 }
 
