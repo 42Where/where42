c3124397cd2c94ffae6977c4b0812034fa0ed681
sunghkim <az0856@naver.com>
2022-11-11 19:43:28 +0900

    [REFACTOR] : CheckApi 리펙터 및 쿠키 로직 리펙터

--- src/main/java/openproject/where42/Oauth/IssueToken.java
+++ src/main/java/openproject/where42/Oauth/IssueToken.java
@@ -2,10 +2,14 @@ package openproject.where42.Oauth;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import openproject.where42.check.AES;
+import openproject.where42.check.MakeCookie;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -15,8 +19,13 @@ import org.springframework.web.client.RestTemplate;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+
+@Controller
+@RequiredArgsConstructor
 public class IssueToken {
 
+	static private AES aes = new AES();
+	static private MakeCookie oven = new MakeCookie();
 	@GetMapping("/invalidRefreshToken")
 	public String invalidRefreshToken(@CookieValue("refresh_token") String refreshToken, HttpServletResponse response) {
 
@@ -29,7 +38,7 @@ public class IssueToken {
 		params.add("grant_type","refresh_token");
 		params.add("client_id","u-s4t2ud-6d1e73793782a2c15be3c0d2d507e679adeed16e50deafcdb85af92e91c30bd0");
 		params.add("client_secret", "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");
-		params.add("refresh_token", refreshToken);
+		params.add("refresh_token", aes.decoding(refreshToken));
 
 		HttpEntity<MultiValueMap<String, String>> authTokenRequest =
 				new HttpEntity<>(params, codeHeaders);
@@ -40,7 +49,6 @@ public class IssueToken {
 				authTokenRequest,
 				String.class
 		);
-		System.out.println(response2.getBody());
 
 		/*** parsing ***/
 		ObjectMapper objectMapper = new ObjectMapper();
@@ -52,10 +60,7 @@ public class IssueToken {
 		}
 
 		/*** save cookie ***/
-		Cookie cookie = new Cookie("access_code", oauthToken.getAccess_token());
-		cookie.setPath("/");
-		cookie.setMaxAge(7200);
-		response.addCookie(cookie);
+		response.addCookie(oven.bakingCookie("access_token", aes.encoding(oauthToken.getAccess_token()), 7200));
 
 		return "/auth/logins";
 	}
