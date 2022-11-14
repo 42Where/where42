974a0acf9e20325ddeab2e0ea544eb048e52a7c0
sunghkim <az0856@naver.com>
2022-11-14 16:59:27 +0900

    [FEAT] : login 로직 추가 및 Token 로직 수정

--- src/main/java/openproject/where42/check/CheckCookie.java
+++ src/main/java/openproject/where42/check/CheckCookie.java
@@ -7,18 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
 
-@Controller
 public class CheckCookie {
 	private static AES aes = new AES();
 	static final private MakeCookie oven = new MakeCookie();
-	
-	@GetMapping("/checkCookie")
-	public String checkCookie(@CookieValue(value = "access_token", required = false) String token,
-							  @CookieValue(value = "refresh_token", required = false) String refreshToken) {
-		if (token != null)
-			return "redirect:/auth/logins/member";
-		if (refreshToken == null)
-			return "redirect:https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-6d1e73793782a2c15be3c0d2d507e679adeed16e50deafcdb85af92e91c30bd0&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Flogin%2Fcallback&response_type=code";
-		return "redirect:/invalidRefreshToken";
-	}
 }
--- src/main/java/openproject/where42/member/LoginController.java
+++ src/main/java/openproject/where42/member/LoginController.java
@@ -69,12 +69,13 @@ public class LoginController {
         return "redirect:/auth/logins";
     }
 
-    //암복호화 테스트용
-//    @GetMapping("/cookietest")
-//    public String t(@CookieValue("access_token") String token) {
-//        AES aes = new AES();
-//        System.out.println(tokenDAO.getAccess_token());
-//        System.out.println(aes.decoding(token));
-//        return "/main_test";
-//    }
+    @GetMapping("/checkCookie")
+    public String checkCookie(@CookieValue(value = "access_token", required = false) String token,
+                              @CookieValue(value = "refresh_token", required = false) String refreshToken) {
+        if (token != null)
+            return "redirect:/auth/logins/member";
+        if (refreshToken == null)
+            return "redirect:https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-6d1e73793782a2c15be3c0d2d507e679adeed16e50deafcdb85af92e91c30bd0&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Flogin%2Fcallback&response_type=code";
+        return "redirect:/invalidRefreshToken";
+    }
 }
