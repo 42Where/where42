29a29e2f15af6348a66ffa48459afeb08baa6635
lamPolar <lovast7@gmail.com>
2022-11-15 18:37:24 +0900

    [FEAT] : Agree page (desktop css)

--- src/main/where42/src/Agree/Agree.js
+++ src/main/where42/src/Agree/Agree.js
@@ -10,13 +10,27 @@ function Agree()
     //api post : login, location, image_url (메인 호출하면 이정보가 오는데, 그걸 다시 보내줘야함)
 
     function Common(){
+        const xmlhttp = new XMLHttpRequest();
+        let   contents = null;
+
+        xmlhttp.open("GET", "./wiki.txt", false);
+        xmlhttp.overrideMimeType("text/html;charset=utf-8");
+        xmlhttp.send();
+        if (xmlhttp.status === 200) {
+            contents = xmlhttp.responseText;
+        }
+        const AgreeClick=()=>{
+            //axios api호출
+        }
         return (
-            <div id="AgreeForm">
-                <div id="AgreeContent"></div>
-                <div id="AgreeSign">
-                    동의하시겠습니까?
-                    <input type="checkbox" id="AgreeCheck"></input>
-                    <button id="FinishButton" type="submit"/>
+            <div id="Wrapper">
+                <div id="HeaderWrapper">
+                    <div id="AgreeHeader">개인 정보 이용 동의서</div>
+                </div>
+                <div id="AgreeContent">{contents}</div>
+                <div id="AskWrapper">
+                    <div id="AgreeAsk">개인정보 이용에 동의하시겠습니까?</div>
+                    <button id="AgreeCheck" onClick={AgreeClick}>동의</button>
                 </div>
             </div>
         )
