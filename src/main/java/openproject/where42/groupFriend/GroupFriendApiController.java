9dd608d645762d4d861c373412e7b062c898baa8
highcho <highjcho@gmail.com>
2022-11-15 18:32:56 +0900

    [FEAT] : hane 토큰 활용하여 검사 로직 수정

--- src/main/java/openproject/where42/groupFriend/GroupFriendApiController.java
+++ src/main/java/openproject/where42/groupFriend/GroupFriendApiController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
